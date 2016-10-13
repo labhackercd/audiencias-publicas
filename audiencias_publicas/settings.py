33db0582195614165420f770571693222acc5cc4
erivanio <erivanio.vanin@gmail.com>
2016-10-13 14:45:51 -0300

    add templates dir

--- audiencias_publicas/settings.py
+++ audiencias_publicas/settings.py
@@ -88,7 +88,7 @@ ROOT_URLCONF = 'audiencias_publicas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
