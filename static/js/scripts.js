73622ac691645bea91a57f926bc32b489e542edd
cfviotti <caio@viotti.me>
2016-10-24 19:41:36 -0200

    Fix nav bar hide script in home

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,14 +1,14 @@
 //Hide and show nav bar when on home
 $(document).ready(function() {
   var aboutHeight = $('.section--home').outerHeight();
-  var scroll = $(window).scrollTop();
 
   if ($(window).scrollTop() >= aboutHeight) {
     $('.navigation.navigation--hidden').addClass('show-translate');
   }
 
   $(window).scroll(function() {
-
+    var scroll = $(window).scrollTop();
+    
     if (scroll >= aboutHeight) {
       $('.navigation.navigation--hidden').addClass('show-translate');
 
