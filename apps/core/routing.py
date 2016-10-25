d3de76047cb2704d09bd313d413a66a55724c486
erivanio <erivanio.vanin@gmail.com>
2016-10-25 11:44:48 -0200

    add link to home in navigation and change path url from video detail

--- apps/core/routing.py
+++ apps/core/routing.py
@@ -6,16 +6,16 @@ channel_routing = [
     route("websocket.disconnect", home.on_disconnect, path=r'^/$'),
 
     route("websocket.connect", chat.on_connect,
-          path=r'^/salas/(?P<pk>\d+)/chat/stream/$'),
+          path=r'^/sala/(?P<pk>\d+)/chat/stream/$'),
     route("websocket.receive", chat.on_receive,
-          path=r'^/salas/(?P<pk>\d+)/chat/stream/$'),
+          path=r'^/sala/(?P<pk>\d+)/chat/stream/$'),
     route("websocket.disconnect", chat.on_disconnect,
-          path=r'^/salas/(?P<pk>\d+)/chat/stream/$'),
+          path=r'^/sala/(?P<pk>\d+)/chat/stream/$'),
 
     route("websocket.connect", questions.on_connect,
-          path=r'^/salas/(?P<pk>\d+)/questions/stream/$'),
+          path=r'^/sala/(?P<pk>\d+)/questions/stream/$'),
     route("websocket.receive", questions.on_receive,
-          path=r'^/salas/(?P<pk>\d+)/questions/stream/$'),
+          path=r'^/sala/(?P<pk>\d+)/questions/stream/$'),
     route("websocket.disconnect", questions.on_disconnect,
-          path=r'^/salas/(?P<pk>\d+)/questions/stream/$'),
+          path=r'^/sala/(?P<pk>\d+)/questions/stream/$'),
 ]
