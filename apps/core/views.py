3cf33b9d2001c5ce41e9c88134a6b82947f37fdd
erivanio <erivanio.vanin@gmail.com>
2016-10-26 16:14:35 -0200

    add agendas in home

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,9 +1,10 @@
 from django.conf import settings
-from apps.core.models import Video
+from apps.core.models import Video, Agenda
 from apps.core.utils import encrypt
 from django.views.generic import TemplateView, DetailView
 import requests
 import json
+from datetime import datetime
 
 
 def receive_callback(request=None):
@@ -36,6 +37,10 @@ class HomeView(TemplateView):
             closed_date__isnull=False).order_by('-published_date')[:5]
         context['live_videos'] = Video.objects.filter(
             closed_date__isnull=True).order_by('-published_date')
+        context['agendas'] = Agenda.objects.filter(
+            situation__startswith='Convocada',
+            session__icontains='Audiência Pública',
+            date__gte=datetime.now()).order_by('date')
         context['no_offset_top'] = 'no-offset-top'
         context['hidden_nav'] = 'navigation--hidden'
         return context
