49597c96d32f31e060b56dc06c929f4f99992dea
erivanio <erivanio.vanin@gmail.com>
2016-10-27 12:00:56 -0200

    add url to callback notification

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,20 +5,16 @@ from apps.core.api import (api_root, AgendaListAPI, MessageListAPI,
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^sala/(?P<pk>\d+)/',
-        VideoDetail.as_view(), name='video_room'),
+    url(r'^sala/(?P<pk>\d+)/', VideoDetail.as_view(), name='video_room'),
+    url(r'^notification/callback/', 'apps.core.views.receive_callback',
+        name='receive_callback'),
 ]
 
 urlpatterns += [
     url(r'^api/$', api_root),
-    url(r'^api/agenda/$', AgendaListAPI.as_view(),
-        name='agenda_list_api'),
-    url(r'^api/messages/$', MessageListAPI.as_view(),
-        name='message_list_api'),
-    url(r'^api/question/$', QuestionListAPI.as_view(),
-        name='question_list_api'),
-    url(r'^api/video/$', VideoListAPI.as_view(),
-        name='video_list_api'),
-    url(r'^api/vote/$', VoteListAPI.as_view(),
-        name='vote_list_api'),
+    url(r'^api/agenda/$', AgendaListAPI.as_view(), name='agenda_list_api'),
+    url(r'^api/messages/$', MessageListAPI.as_view(), name='message_list_api'),
+    url(r'^api/question/$', QuestionListAPI.as_view(), name='question_list_api'),
+    url(r'^api/video/$', VideoListAPI.as_view(), name='video_list_api'),
+    url(r'^api/vote/$', VoteListAPI.as_view(), name='vote_list_api'),
 ]
