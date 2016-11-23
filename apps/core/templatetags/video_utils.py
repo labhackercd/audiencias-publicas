3fa70121adec5d09f71b0b71bdacec45ef1a9c40
cfviotti <caio@viotti.me>
2016-11-23 18:33:54 -0200

    Remove vote button from question when video is closed

--- apps/core/templatetags/video_utils.py
+++ apps/core/templatetags/video_utils.py
@@ -23,17 +23,20 @@ def vote_action(context, question, user):
             'extra_classes': 'voted disabled',
             'total_votes': question.votes_count,
             'extra_attributes': 'disabled',
+            'object': context['object'],
             'upvote_content': 'Sua Pergunta'})
     elif question.votes.filter(user__username=user).count() > 0:
         html = render_to_string('includes/question_action.html', {
             'extra_classes': 'voted',
             'question_vote': 'question-vote',
             'total_votes': question.votes_count,
+            'object': context['object'],
             'upvote_content': 'Apoiada por você'})
     else:
         html = render_to_string('includes/question_action.html', {
             'question_vote': 'question-vote',
             'total_votes': question.votes_count,
+            'object': context['object'],
             'upvote_content': 'Votar Nesta Pergunta'})
     return mark_safe(html)
 
