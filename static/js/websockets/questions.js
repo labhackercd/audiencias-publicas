662158fb4a3cfabb1ad1a201e411b2bc2cf82c6a
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-11-23 11:37:45 -0200

    Fixed share websocket bug
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- static/js/websockets/questions.js
+++ static/js/websockets/questions.js
@@ -43,6 +43,7 @@ questionSocket.onmessage = function(message) {
     }
     var newElement = $(`[data-question-id=${data.id}]`);
     newElement.find('.question-vote').on('click', upVote);
+    newElement.find('.questions__share-link').on('click', shareClick);
     var upvoteButton = newElement.find('.vote-block__upvote-button');
     var totalVotes = newElement.find('.vote-block__total-votes');
 
@@ -101,7 +102,31 @@ function upVote() {
   }
 }
 
+function shareClick() {
+  var pathname = window.location.pathname;
+  var url = window.location.href;
+  url = url.replace(location.hash, '');
+  var question = $(this).closest('.questions__item')
+  var questionPath = question.attr('data-question-path');
+  url = url.replace(pathname, questionPath);
+  var window_opts = 'height=500,width=1000,left=100,top=100,resizable=yes,scrollbars=yes,' +
+                    'toolbar=yes,menubar=no,location=no,directories=no, status=yes'
+
+  if ($(this).find('.icon-facebook').length) {
+    window.open('http://www.facebook.com/sharer/sharer.php?u=' + url,'popUpWindow', window_opts);
+  } else if ($(this).find('.icon-twitter').length) {
+    var twitter_url = `http://twitter.com/share?text=Apoie esta pergunta!&url=${url}`;
+    window.open(twitter_url, 'popUpWindow', window_opts);
+  } else if ($(this).find('.icon-whatsapp').length) {
+    var message = encodeURIComponent('As perguntas mais votadas serão respondidas pelos deputados agora! Acesse em ') + encodeURIComponent(url);
+    window.open('whatsapp://send?text=' + message)
+  }
+
+  return false;
+}
+
 $(".question-vote").on('click', upVote);
+$('.questions__share-link').on('click', shareClick);
 
 questionSocket.onopen = function() { console.log("Connected to question socket"); }
 questionSocket.onclose = function() { console.log("Disconnected to question socket"); }
