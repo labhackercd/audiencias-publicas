6932a61dfebf0936e7b7cdadbf4c2758908ad818
erivanio <erivanio.vanin@gmail.com>
2016-11-23 15:49:03 -0200

    add filters and search in admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -7,8 +7,31 @@ class AgendaAdmin(admin.ModelAdmin):
     list_filter = ['date', 'session', 'situation']
 
 
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'video', 'message', 'created')
+    list_filter = ['user', 'video', 'created']
+    search_fields = ['message']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'video', 'question', 'created')
+    list_filter = ['user', 'video', 'created']
+    search_fields = ['question']
+
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date', 'closed_date', 'created')
+    list_filter = ['created', 'closed_date']
+    search_fields = ['title', 'description', 'videoId']
+
+
+class UpDownVoteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'created')
+    list_filter = ['user', 'question', 'created']
+
+
 admin.site.register(Agenda, AgendaAdmin)
-admin.site.register(Message)
-admin.site.register(Question)
-admin.site.register(Video)
-admin.site.register(UpDownVote)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Video, VideoAdmin)
+admin.site.register(UpDownVote, UpDownVoteAdmin)
