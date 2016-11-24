66edb34f3b242d6376a2e8b2682649b4d5a04fa8
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-11-24 18:39:44 -0200

    Added URL_PREFIX settings
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- apps/core/routing.py
+++ apps/core/routing.py
@@ -1,26 +1,32 @@
+from django.conf import settings
 from channels import route
 from .consumers import home, chat, questions, room_questions
 
+if settings.URL_PREFIX:
+    prefix = r'^/%s' % (settings.URL_PREFIX)
+else:
+    prefix = r'^'
+
 channel_routing = [
-    route("websocket.connect", home.on_connect, path=r'^/$'),
-    route("websocket.disconnect", home.on_disconnect, path=r'^/$'),
+    route("websocket.connect", home.on_connect, path=prefix + r'/$'),
+    route("websocket.disconnect", home.on_disconnect, path=prefix + r'/$'),
 
     route("websocket.connect", chat.on_connect,
-          path=r'^/sala/(?P<pk>\d+)/chat/stream/$'),
+          path=prefix + r'/sala/(?P<pk>\d+)/chat/stream/$'),
     route("websocket.receive", chat.on_receive,
-          path=r'^/sala/(?P<pk>\d+)/chat/stream/$'),
+          path=prefix + r'/sala/(?P<pk>\d+)/chat/stream/$'),
     route("websocket.disconnect", chat.on_disconnect,
-          path=r'^/sala/(?P<pk>\d+)/chat/stream/$'),
+          path=prefix + r'/sala/(?P<pk>\d+)/chat/stream/$'),
 
     route("websocket.connect", questions.on_connect,
-          path=r'^/sala/(?P<pk>\d+)/questions/stream/$'),
+          path=prefix + r'/sala/(?P<pk>\d+)/questions/stream/$'),
     route("websocket.receive", questions.on_receive,
-          path=r'^/sala/(?P<pk>\d+)/questions/stream/$'),
+          path=prefix + r'/sala/(?P<pk>\d+)/questions/stream/$'),
     route("websocket.disconnect", questions.on_disconnect,
-          path=r'^/sala/(?P<pk>\d+)/questions/stream/$'),
+          path=prefix + r'/sala/(?P<pk>\d+)/questions/stream/$'),
 
     route("websocket.connect", room_questions.on_connect,
-          path=r'^/sala/(?P<pk>\d+)/perguntas/questions/stream/$'),
+          path=prefix + r'/sala/(?P<pk>\d+)/perguntas/questions/stream/$'),
     route("websocket.disconnect", room_questions.on_disconnect,
-          path=r'^/sala/(?P<pk>\d+)/perguntas/questions/stream/$'),
+          path=prefix + r'/sala/(?P<pk>\d+)/perguntas/questions/stream/$'),
 ]
