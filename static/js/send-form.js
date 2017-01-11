646a97fd1361b214dd67799cd29b0e5ded7d8167
Cristiano Santos <cristianorsantos.95@gmail.com>
2017-01-11 15:00:29 -0200

    Create send-form.js

---
+++ static/js/send-form.js
@@ -0,0 +1,40 @@
+function sendFormAP($wrapper) { // eslint-disable-line no-unused-vars
+  const elements = {
+    $wrapper,
+    $form: $wrapper.find('[class^="send-form--"]'),
+    $formInput: $wrapper.find('.send-form__input'),
+  };
+
+  function createClosedFormEl() {
+    const closedFormEl = document.createElement('div');
+    const closedFormSpanEl = document.createElement('span');
+
+    closedFormEl.className = 'send-form--closed';
+    closedFormSpanEl.innerHTML = 'Audiência encerrada para participações.';
+
+    closedFormEl.appendChild(closedFormSpanEl);
+    elements.$wrapper[0].appendChild(closedFormEl);
+  }
+
+  function closeForm() {
+    createClosedFormEl();
+    elements.$form.remove();
+  }
+
+  const events = {
+    formInputKeyDown(event) {
+      if (event.which === 13) event.preventDefault();
+    },
+
+    formInputKeyUp(event) {
+      if (event.which === 13) elements.$form.trigger('submit');
+    },
+  };
+
+  (function bindEventsHandlers() {
+    elements.$formInput.on('keydown', events.formInputKeyDown);
+    elements.$formInput.on('keyup', events.formInputKeyUp);
+  }());
+
+  return { closeForm };
+}
