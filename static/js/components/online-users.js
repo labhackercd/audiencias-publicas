771bb1d57cb683b42e73e02378713a175770f187
Cristiano Santos <cristianorsantos.95@gmail.com>
2017-01-23 14:40:35 -0200

    Add online users counter

---
+++ static/js/components/online-users.js
@@ -0,0 +1,20 @@
+/* global videoApiUrl */
+function onlineUsersComponent() {
+  const onlineUsersEl = document.getElementById('online-users');
+
+  function update(usersCount) {
+    onlineUsersEl.innerHTML = usersCount;
+  }
+
+  function get() {
+    $.get(videoApiUrl, (data) => {
+      update(data.online_users);
+    });
+  }
+
+  setInterval(() => get(), 5000);
+
+  return { get };
+}
+
+export default onlineUsersComponent;
--- static/js/pages/room.js
+++ static/js/pages/room.js
@@ -3,9 +3,17 @@ import questionsComponent from '../components/questions';
 import chatComponent from '../components/chat';
 import tabsNavComponent from '../components/tabs-nav';
 import characterCounterComponent from '../components/character-counter';
+import onlineUsersComponent from '../components/online-users';
+
+const onlineUsers = onlineUsersComponent();
 
 const questionsSocket = createSocketHelper('questions', 'questions/stream/');
 const chatSocket = createSocketHelper('chat', 'chat/stream/');
+
+chatSocket.socket.onopen = () => {
+  console.log('Connected to chat socket'); // eslint-disable-line no-console
+  onlineUsers.get();
+};
 
 questionsComponent(questionsSocket.socket);
 chatComponent(chatSocket.socket);
