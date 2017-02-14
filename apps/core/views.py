d0a0f1e44c442936558d9890a299d3bd2cfba3bc
erivanio <erivanio.vanin@gmail.com>
2017-02-14 11:34:56 -0200

    add def to associate youtube video to room and fix querysets

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,13 +1,33 @@
 from django.conf import settings
 from django.http import HttpResponse
 from django.contrib.sites.models import Site
-from apps.core.models import Agenda, Video, Question
+from apps.core.models import Agenda, Video, Question, Room
 from apps.core.utils import encrypt
 from django.views.generic import DetailView, ListView
 import requests
 import json
 from datetime import datetime
 from django.shortcuts import render
+
+
+def associate_videos(video):
+    params = {'part': 'snippet,id',
+              'channelId': settings.YOUTUBE_CHANNEL_ID,
+              'id': video.videoId,
+              'key': settings.YOUTUBE_API_KEY}
+    response = requests.get('https://www.googleapis.com/youtube/v3/videos',
+                            params=params)
+    data = json.loads(response.text)
+    tags = data['items'][0]['snippet']['tags']
+    for tag in tags:
+        if 'IDSessaoReuniao' in tag:
+            cod = tag.split(' ')[1]
+            try:
+                room = Room.objects.get(cod_reunion=cod)
+                room.video_id = video.id
+                room.save()
+            except Room.DoesNotExist:
+                pass
 
 
 def receive_callback(request=None):
@@ -31,15 +51,16 @@ def receive_callback(request=None):
         video.thumb_medium = item['snippet']['thumbnails']['medium']['url']
         video.thumb_high = item['snippet']['thumbnails']['high']['url']
         video.save()
+        associate_videos(video)
     return HttpResponse('<h1>Receive callback</h1>', status=200)
 
 
 def index(request):
     return render(request, 'index.html', context=dict(
-        closed_videos=Video.objects.filter(
-            closed_date__isnull=False).order_by('-published_date')[:5],
-        live_videos=Video.objects.filter(
-            closed_date__isnull=True).order_by('-published_date'),
+        closed_videos=Room.objects.filter(
+            video__closed_date__isnull=False).order_by('-video__published_date')[:5],
+        live_videos=Room.objects.filter(
+            video__closed_date__isnull=True).order_by('-video__published_date'),
         agendas=Agenda.objects.filter(
             situation__startswith='Convocada',
             session__icontains='Audiência Pública',
@@ -48,7 +69,7 @@ def index(request):
 
 
 class VideoDetail(DetailView):
-    model = Video
+    model = Room
     template_name = 'room.html'
 
     def get_context_data(self, **kwargs):
@@ -66,17 +87,17 @@ class VideoDetail(DetailView):
 
 
 class ClosedVideos(ListView):
-    model = Video
+    model = Room
     template_name = 'video-list.html'
 
     def get_queryset(self):
-        return Video.objects.filter(
-            closed_date__isnull=False
+        return Room.objects.filter(
+            video__closed_date__isnull=False
         ).order_by('-published_date')
 
 
 class RoomQuestionList(DetailView):
-    model = Video
+    model = Room
     template_name = 'room_questions_list.html'
 
     def get_context_data(self, **kwargs):
