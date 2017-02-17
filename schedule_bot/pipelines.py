81b34d3eaf154d175da634329622c5c4b9389d25
erivanio <erivanio.vanin@gmail.com>
2017-02-17 17:43:33 -0200

    fix scrapy to get or create agenda

--- schedule_bot/pipelines.py
+++ schedule_bot/pipelines.py
@@ -1,5 +1,58 @@
 # -*- coding: utf-8 -*-
+from django.forms import model_to_dict
+
+
+def item_to_model(item):
+    model_class = getattr(item, 'django_model')
+    if not model_class:
+        raise TypeError("Item is not a `DjangoItem` or is misconfigured")
+
+    return item.instance
+
+
+def get_or_create(model):
+    model_class = type(model)
+    created = False
+
+    # Normally, we would use `get_or_create`. However, `get_or_create` would
+    # match all properties of an object (i.e. create a new object
+    # anytime it changed) rather than update an existing object.
+    #
+    # Instead, we do the two steps separately
+    try:
+        # We have no unique identifier at the moment; use the name for now.
+        obj = model_class.objects.get(cod_reunion=model.cod_reunion)
+    except model_class.DoesNotExist:
+        created = True
+        obj = model  # DjangoItem created a model for us.
+
+    return (obj, created)
+
+
+def update_model(destination, source, commit=True):
+    pk = destination.pk
+
+    source_dict = model_to_dict(source)
+    for (key, value) in source_dict.items():
+        setattr(destination, key, value)
+
+    setattr(destination, 'pk', pk)
+
+    if commit:
+        destination.save()
+
+    return destination
+
+
 class ScheduleBotPipeline(object):
     def process_item(self, item, spider):
-        item.save()
+        try:
+            item_model = item_to_model(item)
+        except TypeError:
+            return item
+
+        model, created = get_or_create(item_model)
+
+        update_model(model, item_model)
+
         return item
--- schedule_bot/spiders/agenda.py
+++ schedule_bot/spiders/agenda.py
@@ -3,7 +3,6 @@ import scrapy
 from schedule_bot.items import ScheduleBotItem
 from scrapy.selector import Selector
 from datetime import datetime
-from apps.core.models import Agenda
 
 
 class AgendaSpider(scrapy.Spider):
@@ -14,7 +13,6 @@ class AgendaSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-        Agenda.objects.all().delete()
         for sessionbox in Selector(response=response).xpath('//div[@class="sessionBox"]'):
             for line in sessionbox.xpath('table/tbody[1]/tr'):
                 commission = sessionbox.xpath('h4/text()').re('[^\t\n\r]+')[0]
