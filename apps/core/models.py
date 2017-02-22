9c3b0d4896bfd76a8936c12e1534aa833f0f2fb1
erivanio <erivanio.vanin@gmail.com>
2017-02-22 11:36:01 -0300

    fix post save agenda to create room

--- apps/core/models.py
+++ apps/core/models.py
@@ -223,13 +223,10 @@ def notification(subject, html, email_list):
 
 
 def agenda_post_save(sender, instance, created, **kwargs):
-    if 'Audiência' in instance.session:
-        if 'Convocada' in instance.situation or \
-           'Andamento' in instance.situation:
-            room = Room.objects.get_or_create(
-                cod_reunion=instance.cod_reunion)[0]
-            room.agenda = instance
-            room.save()
+    if 'Convocada' in instance.situation or 'Andamento' in instance.situation:
+        room = Room.objects.get_or_create(cod_reunion=instance.cod_reunion)[0]
+        room.agenda = instance
+        room.save()
 
 
 def video_pre_save(signal, instance, sender, **kwargs):
