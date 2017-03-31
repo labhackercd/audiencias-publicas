2a13e9d134c683a0cd071987623e196cd7a5b0f4
erivanio <erivanio.vanin@gmail.com>
2017-03-31 16:03:57 -0300

    refactor room admin to new model

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -4,41 +4,13 @@ from apps.core.models import Agenda, Message, Question, Video, UpDownVote, Room
 
 class RoomAdmin(admin.ModelAdmin):
     list_display = (
-        'get_comission', 'get_session', 'get_situation',
-        'get_video', 'is_visible')
-    list_filter = ['is_visible', 'agenda__situation', 'agenda__session']
+        'title_reunion', 'legislative_body_alias', 'youtube_id',
+        'date', 'is_visible')
+    list_filter = [
+        'reunion_status', 'legislative_body_type', 'is_live', 'date']
     search_fields = (
-        'agenda__commission', 'agenda__session', 'agenda__situation',
-        'agenda__date', 'agenda__created', 'video__title', 'video__videoId')
-
-    def get_video(self, obj):
-        if obj.video:
-            return obj.video.videoId
-        else:
-            return 'Sem vídeo'
-
-    def get_comission(self, obj):
-        if obj.agenda:
-            return obj.agenda.commission
-        else:
-            return 'Sala não agendada'
-
-    def get_session(self, obj):
-        if obj.agenda:
-            return obj.agenda.session
-        else:
-            return 'Sala não agendada'
-
-    def get_situation(self, obj):
-        if obj.agenda:
-            return obj.agenda.situation
-        else:
-            return 'Sala não agendada'
-
-    get_video.short_description = 'Vídeo'
-    get_comission.short_description = 'Comissão'
-    get_session.short_description = 'Sessão'
-    get_situation.short_description = 'Situação'
+        'title_reunion', 'legislative_body_initials', 'legislative_body_alias',
+        'legislative_body', 'subcommission', 'location', 'cod_reunion')
 
 
 class AgendaAdmin(admin.ModelAdmin):
