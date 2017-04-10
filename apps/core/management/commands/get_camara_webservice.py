1b9aa201a2469d479b3528825fa6735532614cc7
erivanio <erivanio.vanin@gmail.com>
2017-04-10 11:40:28 -0300

    move function from views to command

--- apps/core/management/commands/get_camara_webservice.py
+++ apps/core/management/commands/get_camara_webservice.py
@@ -1,7 +1,42 @@
 from django.core.management.base import BaseCommand
-from apps.core.views import receive_camara_callback
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from apps.core.models import Room
+import requests
+import json
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        receive_camara_callback()
+        initial_date = datetime.today()
+        final_date = datetime.today() + relativedelta(months=3)
+        params = {'dataInicial': initial_date.strftime('%d/%m/%Y'),
+                  'dataFinal': final_date.strftime('%d/%m/%Y'),
+                  'codComissao': '0',
+                  'bolEdemocracia': '1'}
+        response = requests.get(
+            'https://infoleg.camara.leg.br/ws-pauta/evento/interativo',
+            params=params, verify=False)
+        data = json.loads(response.text)
+        for item in data:
+            if item['codReuniao'] == item['codReuniaoPrincipal']:
+                room, created = Room.objects.get_or_create(
+                    cod_reunion=item['codReuniao'])
+                room.title_reunion = item['txtTituloReuniao']
+                room.legislative_body_initials = item['txtSiglaOrgao']
+                room.legislative_body_alias = item['txtApelido']
+                room.legislative_body = item['txtNomeOrgao']
+                room.subcommission = item['txtNomeSubcomissao']
+                room.reunion_status = item['codEstadoReuniao']
+                room.reunion_type = item['txtTipoReuniao']
+                room.reunion_object = item['txtObjeto']
+                room.location = item['txtLocal']
+                room.legislative_body_type = item['codTipoOrgao']
+                room.is_live = item['bolTransmissaoEmAndamento']
+                room.youtube_id = item['idYoutube']
+                room.is_visible = item['bolHabilitarEventoInterativo']
+                room.youtube_status = item['codEstadoTransmissaoYoutube']
+                date = datetime.strptime(item['datReuniaoString'],
+                                         '%d/%m/%Y %H:%M:%S')
+                room.date = date
+                room.save()
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,50 +1,11 @@
 from django.conf import settings
-from django.http import HttpResponse, Http404
+from django.http import Http404
 from django.utils.translation import ugettext as _
 from django.contrib.sites.models import Site
 from apps.core.models import Question, Room
 from apps.core.utils import encrypt
 from django.views.generic import DetailView, ListView
-import requests
-import json
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 from django.shortcuts import render
-
-
-def receive_camara_callback(request=None):
-    initial_date = datetime.today()
-    final_date = datetime.today() + relativedelta(months=3)
-    params = {'dataInicial': initial_date.strftime('%d/%m/%Y'),
-              'dataFinal': final_date.strftime('%d/%m/%Y'),
-              'codComissao': '0',
-              'bolEdemocracia': '1'}
-    response = requests.get(
-        'https://infoleg.camara.leg.br/ws-pauta/evento/interativo',
-        params=params, verify=False)
-    data = json.loads(response.text)
-    for item in data:
-        if item['codReuniao'] == item['codReuniaoPrincipal']:
-            room, created = Room.objects.get_or_create(
-                cod_reunion=item['codReuniao'])
-            room.title_reunion = item['txtTituloReuniao']
-            room.legislative_body_initials = item['txtSiglaOrgao']
-            room.legislative_body_alias = item['txtApelido']
-            room.legislative_body = item['txtNomeOrgao']
-            room.subcommission = item['txtNomeSubcomissao']
-            room.reunion_status = item['codEstadoReuniao']
-            room.reunion_type = item['txtTipoReuniao']
-            room.reunion_object = item['txtObjeto']
-            room.location = item['txtLocal']
-            room.legislative_body_type = item['codTipoOrgao']
-            room.is_live = item['bolTransmissaoEmAndamento']
-            room.youtube_id = item['idYoutube']
-            room.is_visible = item['bolHabilitarEventoInterativo']
-            room.youtube_status = item['codEstadoTransmissaoYoutube']
-            date = datetime.strptime(item['datReuniaoString'], '%d/%m/%Y %H:%M:%S')
-            room.date = date
-            room.save()
-    return HttpResponse('<h1>Receive callback</h1>', status=200)
 
 
 def index(request):
