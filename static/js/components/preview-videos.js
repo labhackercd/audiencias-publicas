087b4dc076404e887cffeb2001500c5b8e72b1a2
erivanio <erivanio.vanin@gmail.com>
2017-04-26 14:11:03 -0300

    fix live videos bar by js

--- static/js/components/preview-videos.js
+++ static/js/components/preview-videos.js
@@ -13,7 +13,7 @@ function previewVideosComponent(socket) {
   function evaluateSocketMessage(message) {
     const data = JSON.parse(message.data);
     const $video = $(`[data-video-id=${data.id}]`);
-
+    
     if (data.deleted) {
       $video.remove();
       return;
@@ -21,34 +21,36 @@ function previewVideosComponent(socket) {
 
     const videoExists = $video.length;
 
-    if (videoExists && !data.is_closed) {
-      $video.replaceWith(data.html);
-    } else if (videoExists && data.is_closed) {
-      $video.remove();
+    if (data.html.trim().length) {
+      if (videoExists && !data.is_closed) {
+        $video.replaceWith(data.html);
+      } else if (videoExists && data.is_closed) {
+        $video.remove();
 
-      if (elements.$liveList.children().length === 0) {
-        elements.$live.addClass('hide');
+        if (elements.$liveList.children().length === 0) {
+          elements.$live.addClass('hide');
+        }
+
+        if (elements.$closedList.children().length === 0) {
+          elements.$closed.removeClass('hide');
+        } else if (elements.$closedList.children().length >= 5) {
+          elementsVar.$closedLastVideo().remove();
+        }
+
+        elements.$closedList.prepend(data.html);
+      } else if(!videoExists && !data.is_closed) {
+        if (elements.$liveList.children().length === 0) {
+          elements.$live.removeClass('hide');
+        }
+
+        elements.$liveList.prepend(data.html);
+      } else if(!videoExists && data.is_closed) {
+        if (elements.$closedList.children().length === 0) {
+          elements.$closed.removeClass('hide');
+        }
+
+        elements.$closedList.prepend(data.html);
       }
-
-      if (elements.$closedList.children().length === 0) {
-        elements.$closed.removeClass('hide');
-      } else if (elements.$closedList.children().length >= 5) {
-        elementsVar.$closedLastVideo().remove();
-      }
-
-      elements.$closedList.prepend(data.html);
-    } else if(!videoExists && !data.is_closed) {
-      if (elements.$liveList.children().length === 0) {
-        elements.$live.removeClass('hide');
-      }
-
-      elements.$liveList.prepend(data.html);
-    } else if(!videoExists && data.is_closed) {
-      if (elements.$closedList.children().length === 0) {
-        elements.$closed.removeClass('hide');
-      }
-
-      elements.$closedList.prepend(data.html);
     }
   }
 
