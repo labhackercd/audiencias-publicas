47cdec30c13b1615c5827c706386fdb36a6d2bb9
erivanio <erivanio.vanin@gmail.com>
2017-05-08 14:02:41 -0300

    add translations and hide unused admin apps

--- audiencias_publicas/urls.py
+++ audiencias_publicas/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
 from apps.core import urls as core_urls
+from rest_framework.authtoken.models import Token
+from django_q.models import Success, Failure, Schedule
+
 
 if settings.URL_PREFIX:
     prefix = r'^%s/' % (settings.URL_PREFIX)
@@ -12,3 +15,10 @@ urlpatterns = [
     url(prefix + r'', include(core_urls)),
     url(prefix + r'admin/', include(admin.site.urls)),
 ]
+
+admin.site.unregister(Token)
+admin.site.unregister(Success)
+admin.site.unregister(Failure)
+admin.site.unregister(Schedule)
+
+admin.site.site_header = 'Audiências Públicas Interativas'
