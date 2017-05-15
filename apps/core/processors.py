ecffb7657a502b84744d0eb9bce2fb82adbd0329
erivanio <erivanio.vanin@gmail.com>
2017-05-15 13:48:30 -0300

    add olark in decouple

--- apps/core/processors.py
+++ apps/core/processors.py
@@ -3,4 +3,5 @@ from django.conf import settings
 
 def analytics(request):
     analytics_id = settings.GOOGLE_ANALYTICS_ID
-    return {'analytics_id': analytics_id}
+    olark_id = settings.OLARK_ID
+    return {'analytics_id': analytics_id, 'olark_id': olark_id}
