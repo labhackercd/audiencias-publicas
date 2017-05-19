6a9897b97118968c7eff19ff82ba5e7daaee9263
erivanio <erivanio.vanin@gmail.com>
2017-05-19 11:41:52 -0300

    change stylesheet from calendar

--- static/js/pages/index.js
+++ static/js/pages/index.js
@@ -6,3 +6,21 @@ const previewVideosSocket = createSocketHelper('preview videos', '');
 previewVideosComponent(previewVideosSocket.socket);
 
 window.onbeforeunload = () => previewVideosSocket.close();
+
+$(".show-more a").on("click", function() {
+  var $this = $(this); 
+  var $contentTruncate = $this.parent().prev().prev("div.content-truncate");
+  var $contentFull = $this.parent().prev("div.content-full");
+  var linkText = $this.text();   
+  if(linkText === "Ver mais"){
+    linkText = "Ver menos";
+    $contentTruncate.addClass("hide");
+    $contentFull.removeClass("hide");
+  } else {
+    linkText = "Ver mais";
+    $contentTruncate.removeClass("hide");
+    $contentFull.addClass("hide");
+  };
+
+  $this.text(linkText);
+});
