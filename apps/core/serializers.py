c4cc9dc46d3556ad62a0d47ed9f02f67294507a8
erivanio <erivanio.vanin@gmail.com>
2017-05-23 15:46:23 -0300

    add date in room serializer

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -60,4 +60,4 @@ class RoomSerializer(serializers.ModelSerializer):
                   'legislative_body_alias', 'youtube_status', 'is_joint',
                   'max_online_users', 'created', 'modified', 'is_visible',
                   'reunion_type', 'title_reunion', 'reunion_object',
-                  'reunion_theme')
+                  'reunion_theme', 'date')
