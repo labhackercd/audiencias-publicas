0c0c82dc030d978be4547ed1df70734e0978025a
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-06-27 16:00:32 -0300

    Added loader
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- static/js/popup-login.js
+++ static/js/popup-login.js
@@ -8,6 +8,7 @@ $('.js-login-button').click(function(e){
       setTimeout(checkWindow, 100);
       return;
     } else {
+      %('.widget-loader').addClass('show');
       document.location.reload(true);
     }
   }
