aa24f7822a9527227b340091d511c3ac8f0ec536
erivanio <erivanio.vanin@gmail.com>
2017-06-30 16:11:46 -0300

    add initials in room api

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -57,7 +57,7 @@ class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('id', 'cod_reunion', 'online_users', 'youtube_id',
-                  'legislative_body_alias', 'youtube_status', 'is_joint',
-                  'max_online_users', 'created', 'modified', 'is_visible',
-                  'reunion_type', 'title_reunion', 'reunion_object',
-                  'reunion_theme', 'date')
+                  'legislative_body_alias', 'legislative_body_initials',
+                  'youtube_status', 'is_joint', 'max_online_users', 'created',
+                  'modified', 'is_visible', 'reunion_type', 'title_reunion',
+                  'reunion_object', 'reunion_theme', 'date')
