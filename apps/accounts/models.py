086bfacb445a5384247e40882d210346f4845f90
erivanio <erivanio.vanin@gmail.com>
2017-07-04 11:59:03 -0300

    change max_length from first_name and last_name

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
+    first_name = models.CharField(_('first name'), max_length=200, blank=True)
+    last_name = models.CharField(_('last name'), max_length=200, blank=True)
+
     class Meta:
         db_table = 'auth_user'
 
