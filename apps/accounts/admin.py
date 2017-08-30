bce5497ce3ff26d4c22b4c7dd545c9711be490f2
erivanio <erivanio.vanin@gmail.com>
2017-08-30 15:18:58 -0300

    add search fields in user profile admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -5,7 +5,9 @@ from apps.accounts.models import UserProfile, User
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'is_admin')
-    list_filter = ['user', 'is_admin']
+    list_filter = ['is_admin', 'user__is_superuser']
+    search_fields = (
+        'user__first_name', 'user__last_name', 'user__email', 'user__username')
 
 
 admin.site.register(User, UserAdmin)
