1e105e9ded0e5bf555fbbbd87e0348946a00d585
erivanio <erivanio.vanin@gmail.com>
2017-10-05 15:57:00 -0300

    :hammer: add recaptcha, fix urls and js classes

--- templates/components/edem/static/edem/js/edem-navigation.js
+++ templates/components/edem/static/edem/js/edem-navigation.js
@@ -37,7 +37,7 @@ document.addEventListener('click', function(e) {
 });
 
 function showError(errorMessage) {
-  $('.JS-accessErrorBox').removeAttr('hidden','');
+  $('.JS-accessErrorBox').removeAttr('hidden');
   $('.JS-accessError').text(errorMessage);
 }
 
@@ -54,7 +54,7 @@ $('.JS-formInput').blur(function() {
   }
 });
 
-$('#id_form_login').submit(function(event) {
+$('.JS-loginForm').submit(function(event) {
   event.preventDefault();
   $.ajax({
     type:"POST",
@@ -112,75 +112,38 @@ $('.JS-closeAccessError').click(function(){
   $('.JS-accessErrorBox').attr('hidden', '');
 });
 
-$('#id_form_validation').submit(function(event) {
+$('.JS-signUpForm').submit(function(event) {
   event.preventDefault();
-  $.ajax({
-    type:"POST",
-    url: '/ajax/validation/',
-    data: $(event.target).serialize(),
-    success: function(response){
-      window.sessionStorage
-        .setItem('userData', JSON.stringify(response['data']));
-      $('.login-box__signup-wrapper').addClass('step-2');
-    },
-    error: function(jqXRH) {
-      if (jqXRH.status == 0) {
-        showError('Verifique sua conexão com a internet.');
-      } else if (jqXRH.status == 400) {
-        $('.JS-inputError').text('');
-        $.each(jqXRH.responseJSON["data"], function(key, value) {
-          if (key != '__all__') {
-            $(event.target)
-              .find('[data-input-name="'+key+'"]')
-              .text(value)
-              .removeAttr('hidden');
-          }
-        });
-      } else {
-        showError("Ocorreu um erro no servidor, tente novamente em alguns instantes.");
-      }
-    }
-  });
-});
-
-$('#id_form_signup').submit(function(event) {
-  event.preventDefault();
-  var signup_form = {}
-  $.map($(event.target).serializeArray(), function(n, i){
-    signup_form[n['name']] = n['value'];
-  });
-  var user_data = $.extend(JSON.parse(sessionStorage.userData), signup_form);
   if (grecaptcha.getResponse() == ""){
-    alert("Por favor preencha o reCAPTCHA.");
+    showError("Por favor preencha o reCAPTCHA.");
   } else {
     $.ajax({
       type:"POST",
       url: '/ajax/signup/',
-      data: user_data,
+      data: $(event.target).serialize(),
       success: function(response){
-        $('.login-box__signup-wrapper').removeClass('step-2').addClass('step-3');
+        alert("Usuário cadastrado com sucesso. Verifique seu email."); // Falta criar local para mensagem de sucesso
       },
       error: function(jqXRH) {
+        grecaptcha.reset();
+        $("#g-recaptcha-response").val("");
         if (jqXRH.status == 0) {
           showError('Verifique sua conexão com a internet.');
         } else if (jqXRH.status == 400) {
           $('.JS-inputError').text('');
           $.each(jqXRH.responseJSON["data"], function(key, value) {
-            if (key == 'email') {
-              $('.login-box__signup-wrapper').removeClass('step-2');
-              $('#id_form_validation')
-                .find('[data-input-name="'+key+'"]')
-                .text(value)
-                .removeAttr('hidden');
-            } else if (key != '__all__') {
+            if (key != '__all__') {
               $(event.target)
                 .find('[data-input-name="'+key+'"]')
                 .text(value)
                 .removeAttr('hidden');
+            } else {
+              showError(value);
             }
           });
+        } else if (jqXRH.status == 401) {
+          showError(jqXRH.responseJSON["data"])
         } else {
-
           showError("Ocorreu um erro no servidor, tente novamente em alguns instantes.");
         }
       }
