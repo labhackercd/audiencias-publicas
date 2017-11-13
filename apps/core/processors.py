0d57d3713d5dcf493dfc38ff38998adb03f1c144
erivanio <erivanio.vanin@gmail.com>
2017-11-13 17:46:16 -0200

    add variable with site name

--- apps/core/processors.py
+++ apps/core/processors.py
@@ -7,3 +7,7 @@ def analytics(request):
     recaptcha_site_key = settings.RECAPTCHA_SITE_KEY
     return {'recaptcha_site_key': recaptcha_site_key,
             'analytics_id': analytics_id, 'olark_id': olark_id}
+
+
+def home_customization(request):
+    return {'site_name': settings.SITE_NAME}
