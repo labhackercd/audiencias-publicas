c556eafd5fb808b45f510400ebb409e97d3da346
erivanio <erivanio.vanin@gmail.com>
2019-07-25 13:28:33 -0300

    change api offset

--- audiencias_publicas/settings.py
+++ audiencias_publicas/settings.py
@@ -85,7 +85,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 100
 }
 
 CAMARA_LOGIN = config('CAMARA_LOGIN', default=False, cast=bool)
