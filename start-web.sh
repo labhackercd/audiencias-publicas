1fe53a4b7dedc20fd4f169c61f4b56d8722fb21f
Thiago Nogueira <thiagonf10@gmail.com>
2020-09-08 12:39:12 -0300

    Fixing enviroment from docker-compose and script start application

--- start-web.sh
+++ start-web.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
-postgres_ready(){
-python3 << END
-import sys
-import psycopg2
-from os import environ
-
-dbname = environ['DATABASE_NAME']
-user = environ['DATABASE_USER']
-password = environ['DATABASE_PASSWORD']
-host = environ['DATABASE_HOST']
-
-try:
-    conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
-except psycopg2.OperationalError:
-    sys.exit(-1)
-sys.exit(0)
-END
-}
-
-until postgres_ready; do
-  >&2 echo "Postgres is unavailable. Waiting."
-  sleep 1
+while true; do
+    PG_STATUS=`PGPASSWORD=$DATABASE_PASSWORD psql -h $DATABASE_HOST -p 5432 -U $DATABASE_USER -w $DATABASE_PASSWORD -c '\l \q'  | grep postgres | wc -l`
+    if ! [ "$PG_STATUS" -eq "0" ]; then
+       break
+    fi
+    echo "Waiting Database Setup"
+    sleep 10
 done
 
+PGPASSWORD=$DATABASE_PASSWORD psql -h $DATABASE_HOST -U $DATABASE_USER -W $DATABASE_PASSWORD -c "CREATE DATABASE ${DATABASE_NAME} OWNER ${DATABASE_USER}"
 
 python3 manage.py migrate
 python3 create_admin.py
