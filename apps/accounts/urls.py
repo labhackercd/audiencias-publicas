76bb1fe3b36eec94e5b2426031a29e2abfba8b90
erivanio <erivanio.vanin@gmail.com>
2021-11-10 15:30:20 -0300

    update urls from django 1.11 to django 3

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,30 +1,27 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 from audiencias_publicas.decorators import anonymous_required
 from apps.accounts import views
 
 urlpatterns = [
-    url(r'^login/$', anonymous_required(auth_views.login), name='login'),
-    url(r'^logout/$', login_required(auth_views.logout_then_login),
-        name='logout'),
-    url(r'^signup/$', anonymous_required(views.SignUpView.as_view()),
-        name='signup'),
-    url(r'^password_change/$', auth_views.password_change,
-        name='password_change'),
-    url(r'^password_change/done/$',
-        auth_views.password_change_done,
-        name='password_change_done'),
-    url(r'^password_reset/$', auth_views.password_reset,
-        name='password_reset'),
-    url(r'^password_reset/done/$',
-        auth_views.password_reset_done,
-        name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/'
-        '(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm,
-        name='password_reset_confirm'),
-    url(r'^reset/done/$',
-        auth_views.password_reset_complete,
-        name='password_reset_complete'),
+    path('login/', anonymous_required(auth_views.LoginView.as_view()),
+         name='login'),
+    path('logout/', login_required(auth_views.logout_then_login),
+         name='logout'),
+    path('signup/', anonymous_required(views.SignUpView.as_view()),
+         name='signup'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
