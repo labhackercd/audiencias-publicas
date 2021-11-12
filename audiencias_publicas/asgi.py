7c23b8e1962b86a2a2c29690750e0a10f57a17c0
erivanio <erivanio.vanin@gmail.com>
2021-11-12 15:52:47 -0300

    changes to use ASGI application

--- audiencias_publicas/asgi.py
+++ audiencias_publicas/asgi.py
@@ -1,5 +1,15 @@
 import os
-import channels.asgi
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "audiencias_publicas.settings")
-channel_layer = channels.asgi.get_channel_layer()
+from django.core.asgi import get_asgi_application
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from apps.core.routing import websocket_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'audiencias_publicas.settings')
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(URLRouter(websocket_urlpatterns)) 
+})
